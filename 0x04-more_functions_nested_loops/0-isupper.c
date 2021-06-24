b30ff48eb337e653fc632f530e251a72e6662f82
DavidGondo <d.gondo@alustudent.com>
2021-06-24 07:08:48 -0700

    Update

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+*int _isupper(int c): Function prototype
+*
+*Return :Upper case(1)
+*Return : otherwise(0)
+*
+*/
+
+int _isupper(int c)
+{
+  int i;
+  if(i>64 && i<91)
+    {
+      _putchar(i);
+      return (1);
+    }else
+    {
+      return (0);
+    }
+
+}
