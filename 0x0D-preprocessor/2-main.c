45f9a96458bed1cf83d3e0ad281ebe8dc5b71c95
DavidGondo <d.gondo@alustudent.com>
2021-07-20 00:12:51 -0700

    Update

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,10 @@
-#ifndef DOUBLEGUARD
-#define DOUBLEGUARD
-#define ABS(x) ((x) < 0 ? ((x) * (-1)) : (x))
-#endif
+#include <stdio.h>
+/**
+* main - prints name of compilation file with new line
+* Return: 0
+**/
+int main(void)
+{
+printf("%s\n", __BASE_FILE__);
+return (0);
+}
