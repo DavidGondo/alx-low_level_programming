972ef8982afa57657e92421e87648546723ef9bb
DavidGondo <d.gondo@alustudent.com>
2021-08-09 04:01:05 -0700

    Update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,33 +14,38 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, read_fun, write_fun;
-	char *tmp;
+	int fd;
+	int s, t;
+	char *buf;
 
-	if (filename == NULL)
-		return (0);
-
-	tmp = malloc(sizeof(char) * letters);
-
-	if (tmp == NULL)
+	if (!filename)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-
-	if (fd == -1)
+	if (fd < 0)
 		return (0);
 
-	read_fun = read(fd, tmp, letters);
-
-	if (read_fun == -1)
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
 		return (0);
 
-	write_fun =  write(STDOUT_FILENO, tmp, read_fun);
-
-	if (write_fun == -1)
+	s = read(fd, buf, letters);
+	if (s < 0)
+	{
+		free(buf);
 		return (0);
+	}
+	buf[s] = '\0';
+
 	close(fd);
 
-	free(tmp);
-	return (write_fun);
+	t = write(STDOUT_FILENO, buf, s);
+	if (t < 0)
+	{
+		free(buf);
+		return (0);
+	}
+
+	free(buf);
+	return (t);
 }
