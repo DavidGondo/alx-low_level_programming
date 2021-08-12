96cc2db6be24e26804e137acb8e6fcc8c780469e
DavidGondo <d.gondo@alustudent.com>
2021-08-12 15:11:22 -0700

    Update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,7 @@
 #include "main.h"
-
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 /**
  * read_textfile - function that reads a text file and
  * prints it to the POSIX standard output
